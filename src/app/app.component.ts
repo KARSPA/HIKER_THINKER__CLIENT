c30b5d14439cb1af03820f0e0d81f4428a33d1d8
KARSPA <kaspar.ivan@outlook.com>
2025-03-11 15:04:02 +0100

    Début frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./_partials/header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, HeaderComponent],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'HikerThinker';
