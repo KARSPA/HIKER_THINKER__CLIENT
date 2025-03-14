ae6afa7da726e08754cb9a74a2f9f37f2ec1c748
KARSPA <kaspar.ivan@outlook.com>
2025-03-14 19:44:46 +0100

    Icones catégories ET interface de la modale

--- src/app/components/add-category-modal/add-category-modal.component.ts
+++ src/app/components/add-category-modal/add-category-modal.component.ts
@@ -1,12 +1,65 @@
 import { Component, Input } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-category-modal',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './add-category-modal.component.html'
 })
 export class AddCategoryModalComponent {
 
   @Input() title : string | undefined;
+
+
+  icons : string[] = [
+    'icon_empty',
+    'icon_backpack',
+    'icon_bed',
+    'icon_bier',
+    'icon_bolt',
+    'icon_boots',
+    'icon_bottle',
+    'icon_camera',
+    'icon_clock',
+    'icon_clothes',
+    'icon_coffee',
+    'icon_fishing',
+    'icon_flashlight',
+    'icon_food',
+    // 'icon_goggles',
+    'icon_health',
+    'icon_heart',
+    'icon_light',
+    'icon_map',
+    'icon_moon',
+    'icon_paw',
+    'icon_recycle',
+    'icon_shower',
+    'icon_star',
+    'icon_sun',
+    'icon_tent',
+    'icon_toiletpaper',
+    'icon_toothbrush',
+    'icon_trash',
+    'icon_walk',
+    'icon_water',
+    'icon_wind',
+    'icon_zzz'
+  ]
+
+  categoryForm: FormGroup = new FormGroup({
+    name: new FormControl('',[Validators.required]),
+    icon: new FormControl('icon_empty',[Validators.required])
+  })
+
+
+  onSubmit(){
+    console.log("SOUMIS !", this.categoryForm.value)
+  }
+
+
+  selectIcon(iconName : string) : void{
+    this.categoryForm.get('icon')?.setValue(iconName);
+  }
 
 }
