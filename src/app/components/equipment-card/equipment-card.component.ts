e0bcaaeee9822749f1702880a22d98f110d3191a
KARSPA <kaspar.ivan@outlook.com>
2025-03-16 19:52:46 +0100

    Lien vers détails depuis l'inventaire

--- src/app/components/equipment-card/equipment-card.component.ts
+++ src/app/components/equipment-card/equipment-card.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { Equipment } from '../../interfaces/equipment/Equipment';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-equipment-card',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './equipment-card.component.html'
 })
 export class EquipmentCardComponent {
