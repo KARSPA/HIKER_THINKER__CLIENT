db1f4a7674b588fe2017e2f5984f97648a291cbe
KARSPA <kaspar.ivan@outlook.com>
2025-03-17 13:45:05 +0100

    Fin accordéon front

--- src/app/pages/inventory/inventory.component.ts
+++ src/app/pages/inventory/inventory.component.ts
@@ -154,8 +154,8 @@ export class InventoryComponent implements OnInit{
 
       if (content && content.style.maxHeight && content.style.maxHeight !== '0px') {
         content.style.maxHeight = '0';
-        content.classList.toggle('border')
         content.classList.toggle('border-stone-300')
+        content.classList.toggle('border')
         
         icon.classList.toggle('rotate-180')
         categoryContainer.classList.toggle('rounded-b-md')
@@ -169,8 +169,8 @@ export class InventoryComponent implements OnInit{
           content.style.maxHeight = categoryContainer.scrollHeight + 'px';
           content.style.height = categoryContainer.scrollHeight + 'px';
         }
-        content.classList.toggle('border')
         content.classList.toggle('border-stone-300')
+        content.classList.toggle('border')
 
         icon.classList.toggle('rotate-180')
         categoryContainer.classList.toggle('rounded-b-md')
