b6c55a5612e625daa5aecb42b211847833a11eeb
KARSPA <kaspar.ivan@outlook.com>
2025-03-17 22:35:11 +0100

    Cablage bouton suppression équipement depuis la page de détails

--- src/app/pages/equipment-details/equipment-details.component.ts
+++ src/app/pages/equipment-details/equipment-details.component.ts
@@ -3,15 +3,21 @@ import { Equipment } from '../../interfaces/equipment/Equipment';
 import { EquipmentService } from '../../services/equipment.service';
 import { Router, RouterLink } from '@angular/router';
 import { EquipmentCardComponent } from '../../components/equipment-card/equipment-card.component';
+import { ModalService } from '../../services/modal.service';
+import { RemoveEquipmentConfirmModalComponent } from '../../components/remove-equipment-confirm-modal/remove-equipment-confirm-modal.component';
+import { InventoryService } from '../../services/inventory.service';
 
 @Component({
   selector: 'app-equipment-details',
   imports: [RouterLink],
   templateUrl: './equipment-details.component.html'
 })
-export class EquipmentDetailsComponent {
+export class EquipmentDetailsComponent implements OnInit{
+  
 
   private equipmentService : EquipmentService = inject(EquipmentService);
+  private inventoryService : InventoryService = inject(InventoryService);
+  private modalService : ModalService = inject(ModalService);
   private router : Router = inject(Router);
 
   equipment !: Equipment;
@@ -28,6 +34,22 @@ export class EquipmentDetailsComponent {
       }
     });
   }
+
+
+  ngOnInit(): void {
+    this.inventoryService.equipmentRemove$.subscribe({
+      next:(equipmentId)=>{
+        this.router.navigate(['/inventory'])
+      }
+    })
+  }
+
+  openRemoveEquipmentConfirmationModal(){
+      this.modalService.openModal({
+        component: RemoveEquipmentConfirmModalComponent,
+        data: {equipment : this.equipment ?? undefined}
+      })
+    }
   
 
 }
