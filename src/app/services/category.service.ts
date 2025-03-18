813d1932ddaf62eb1c7512d49f33f9129aa56201
KARSPA <kaspar.ivan@outlook.com>
2025-03-18 11:41:14 +0100

    Méthodes dans service

--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -10,13 +10,13 @@ import { ResponseModel } from '../interfaces/ResponseModel';
 export class CategoryService {
 
   private INVENTORY_BASE_URL = "http://localhost:8000/api/v1/inventory/categories";
+  private HIKE_BASE_URL = "http://localhost:8000/api/v1/hikes";
 
 
   private httpClient : HttpClient = inject(HttpClient);
 
 
   addInventoryCategory(category : Category) : Observable<ResponseModel<Category>>{
-
     console.log(category)
     return this.httpClient.post<ResponseModel<Category>>(this.INVENTORY_BASE_URL, {
       name : category.name,
@@ -25,7 +25,6 @@ export class CategoryService {
     });
 
   }
-
   modifyInventoryCategory(category : Category) : Observable<ResponseModel<Category>>{
     console.log(category);
 
@@ -35,14 +34,44 @@ export class CategoryService {
       order : category.order
     })
   }
-
-
-
   removeInventoryCategory(categoryId : string) : Observable<ResponseModel<Category>>{
     console.log('CategoryId (remove) : ', categoryId);
 
     return this.httpClient.delete<ResponseModel<Category>>(this.INVENTORY_BASE_URL+`/${categoryId}`);
     
+  }
+
+
+  
+  addHikeCategory(hikeId : string, category : Category) : Observable<ResponseModel<Category>>{
+
+    console.log(category)
+    return this.httpClient.post<ResponseModel<Category>>(this.buildBaseHikeCategoryUrl(hikeId), {
+      name : category.name,
+      icon : category.icon,
+      order : category.order
+    });
+
+  }
+  modifyHikeCategory(hikeId : string, category : Category) : Observable<ResponseModel<Category>>{
+    console.log(category);
+
+    return this.httpClient.patch<ResponseModel<Category>>(this.buildBaseHikeCategoryUrl(hikeId)+`/${category.id}`, {
+      name : category.name,
+      icon : category.icon,
+      order : category.order
+    })
+  }
+  removeHikeCategory(hikeId : string, categoryId : string) : Observable<ResponseModel<Category>>{
+    console.log('CategoryId (remove) : ', categoryId);
+
+    return this.httpClient.delete<ResponseModel<Category>>(this.buildBaseHikeCategoryUrl(hikeId)+`/${categoryId}`);
+    
+  }
+
+
+  private buildBaseHikeCategoryUrl(hikeId : string){
+    return `${this.HIKE_BASE_URL}/${hikeId}/categories`;
   }
 
 }
