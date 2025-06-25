841bb20bd5ea0ad90afb6ba7f908837361ae629f
KARSPA <kaspar.ivan@outlook.com>
2025-06-25 10:55:43 +0200

    Bump correction bug déploiement

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     apiUrl: 'http://localhost:8000/api/v1',
-    appVersion : "v0.3.1",
+    appVersion : "v0.3.2",
     title : "HikerThinker DEV"
   };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     apiUrl: 'https://api.hikerthinker.fr/api/v1',
-    appVersion : "v0.3.1",
+    appVersion : "v0.3.2",
     title : "HikerThinker"
   };
